6fb9851b50b92f341a68fc39244be86cb25ced17
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-09-22 10:38:52 +0200

    click v0

--- import_click/models/account_click.py
+++ import_click/models/account_click.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 
 class AccountClick(models.Model):
-    _name = "import.click"
+    _name = "account.click"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Click import"
 
