a53b195e8d074bd7d048c7d9ea235677ae1919ea
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-04-14 15:15:13 +0200

    gesgru

--- gesgru_importer/models/res_company.py
+++ gesgru_importer/models/res_company.py
@@ -99,7 +99,7 @@ class GesgruImporter(models.Model):
                 template = "- An exception of type {0} occurred. Arguments:\n{1!r}"
                 message = template.format(type(ex).__name__, ex.args)
 
-            raise ValidationError(products)
+        raise ValidationError(products)
 
     def iterateCompanies(self):
         companies = self.env['res.company'].search([('directories', '!=', '')])
